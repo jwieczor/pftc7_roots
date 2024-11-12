61d7d8698e0ac0d28ca5cdc2aa1729dd19de7668
jwieczor <jd.wieczorkowski@gmail.com>
2024-11-12 15:38:50 +0000

    J added amplitude to plotting

--- scripts/script_GPR_relations.R
+++ scripts/script_GPR_relations.R
@@ -14,6 +14,7 @@ gpr <- read.csv("data/GPR/GPR_vs_root_density_cleaned.csv") %>%
 glimpse(gpr)
 
 hist(log(gpr$root_dens_soil))
+hist(log(gpr$Amplitude))
 
 # Custom min-max scaling
 min_val <- min(gpr$Pixel_count)
@@ -24,9 +25,12 @@ gpr$Pixel_count_scaled <- (gpr$Pixel_count - min_val) / (max_val - min_val)
 # Models ----
 model1 <- lm(log(root_dens_soil) ~ Pixel_count, data = gpr)
 model2 <- lm(log(root_dens_soil) ~ Pixel_count_scaled, data = gpr)
+model3 <- lm(log(root_dens_soil) ~ Amplitude, data = gpr)
 
 summary(model1)
 summary(model2) # No difference between the two models
+
+summary(model3)
 
 # Plot ----
 RD_pixel <- ggplot(aes(x = Pixel_count, y = log(root_dens_soil)), data = gpr) +
@@ -34,8 +38,19 @@ RD_pixel <- ggplot(aes(x = Pixel_count, y = log(root_dens_soil)), data = gpr) +
   stat_smooth(method=lm) +
   #facet_wrap(~group, nrow = 2) +
   theme_bw() +
-  labs(x = "\nPixel count", y = "Root density (log)\n") +
+  labs(x = "\nPixel count", y = "\nRoot density (log)\n") +
   theme(panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank())
 
-ggsave(RD_pixel, file = "plots/RD_pixell.png", width = 210, height = 100, units = "mm", dpi = 320)
+RD_amplitude <- ggplot(aes(x = Amplitude, y = log(root_dens_soil)), data = gpr) +
+  geom_point() +
+  stat_smooth(method=lm) +
+  #facet_wrap(~group, nrow = 2) +
+  theme_bw() +
+  labs(x = "\nAmplitude", y = "\nRoot density (log)\n") +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank())
+
+RD_px_am <- gridExtra::grid.arrange(RD_pixel, RD_amplitude, nrow = 1)
+
+ggsave(RD_px_am, file = "plots/RD_px_am.png", width = 210, height = 100, units = "mm", dpi = 320)
