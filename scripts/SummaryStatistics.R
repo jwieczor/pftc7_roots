3c1cd45e0bc612ce698e0a9ac94487a6fa2977bb
Aragon Baquero <lxa1960@miami.edu>
2024-11-13 14:23:51 -0500

    Data Records Summary Statistics

--- scripts/SummaryStatistics.R
+++ scripts/SummaryStatistics.R
@@ -1,50 +1,24 @@
-## Load packages
-pkgs <- c("dplyr", "tidyverse", "stringr", "tidyr")
-lapply(pkgs, library, character.only = TRUE)
-remove(pkgs)
 
+#Load table 
+fT <- read.csv("D:/OneDrive - University of Miami/UMiami/PFTC7/DATA/pftc7_roots/data/roots/processed/BelowgroundTraitsDataset.csv",
+               h=T)
 
-# Load Whole Roots Table 
-ft <- read.csv("D:/OneDrive - University of Miami/UMiami/PFTC7/DATA/pftc7_roots/data/roots/processed/Root_and_leaf_traits_RP_20241029.csv", h = T)
-
-#Reorganize Table Root Traits, Leaf Traits, and Biomass Tratis. Change names to match paper style. Calculate LT
-ftFINAL <-ft %>% 
-  select(File.Name, date, elevation, siteID, aspect, sp, plantID, fire, root_depth, no_root_scan, root_wet_mass_g, 
-         root_dry_mass, Total.Root.Length.mm, Volume.mm3, Average.Diameter.mm, Branching.frequency.per.mm, 
-         SRL, RTD, RDMC, no_tuber_scan, tuber_wet_mass_g, tuber_dry_mass, leafID, height, no_leaf, 
-         leaf_wet_mass_g, leaf_dry_mass, leaf_thick_1_mm, leaf_thick_2_mm, leaf_thick_3_mm,
-         leaf_area, SLA, LDMC, BGB, AGB, AGB.BGB) %>% 
-  rename(ID = File.Name) %>% 
-  rename(reproductive_height_cm = leafID) %>% 
-  rename(vegetative_height_cm = height) %>% 
-  rename(root_depth_cm = root_depth) %>% 
-  rename(root_dry_mass_g = root_dry_mass) %>% 
-  rename(total_root_length_mm = Total.Root.Length.mm) %>% 
-  rename(root_volume_mm3 = Volume.mm3) %>% 
-  rename(root_average_diameter_mm = Average.Diameter.mm) %>% 
-  rename(root_branching_intensity_mm = Branching.frequency.per.mm) %>% 
-  rename(tuber_dry_mass_g = tuber_dry_mass) %>% 
-  rename(leaf_dry_mass_g = leaf_dry_mass) %>% 
-  rename(leaf_area_mm2 = leaf_area) %>% 
-  rename(belowground_biomass_g = BGB) %>% 
-  rename(aboveground_biomass_g = AGB) %>% 
-  rename("ABG:BGB" = AGB.BGB) %>%
-  mutate(fire = ifelse(fire == "",0,1)) %>% 
-  rowwise() %>% 
-  mutate(LT = mean(c_across(starts_with("leaf_thick")), na.rm = F), .after = SLA)
-
-remove
-  
-write.csv(ftFINAL, "D:/OneDrive - University of Miami/UMiami/PFTC7/DATA/pftc7_roots/data/roots/processed/BelowgroundTraitsDataset_20241107.csv",
-          row.names=FALSE)
+#group ftFINAL by elevation and calculate mean and sd
+sumsta_all <- fT %>% 
+  summarise(across(RD:RDMC,
+                   .fns = list(mean = mean, sd = sd),
+                   na.rm = TRUE)) %>% 
+  pivot_longer(everything(), names_sep='_', names_to=c('variable', '.value'))
+sumsta_all
 
 
 
 #group ftFINAL by elevation and calculate mean and sd
-sumsta_elevation <- ftFINAL %>% 
+sumsta_elevation <- fT %>% 
   group_by(elevation) %>% 
-  summarise(across(c("root_average_diameter_mm","root_branching_intensity_mm", "SRL", "RTD", "RDMC"),
+  summarise(across(c("RD","BI", "SRL", "RTD", "RDMC"),
                    .fns = list(mean = mean, sd = sd),
-                   na.rm = TRUE)) 
-  # pivot_longer(everything(), names_sep='_', names_to=c('variable', '.value'))
+                   na.rm = TRUE)) %>% 
+  pivot_longer(!elevation, names_sep='_', names_to=c('variable', '.value')) %>% 
+  arrange(variable,elevation)
 sumsta_elevation
